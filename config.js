72450d5b4eddfb3ab6bf6a34fff0cc2d0b570dd0
Deadman5636 <120655924+Deadman5636@users.noreply.github.com>
2022-12-15 22:09:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
+global.Owner = ['01110640835']
+global.OwnerNumber = ['01110640835']
+global.ownertag = ['01110640835']
+global.BotName = "Bani_Bot"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.OwnerName = "BANI"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
+global.prefa = ['#'] // Prefix ---  change it to anythong you want except @//
 
 
 global.location = "West Bengal, India"
